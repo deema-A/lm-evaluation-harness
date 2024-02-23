fb5fd6df2bc950a8d66fb78b26a6d777d1489256
deema <deemaazizn@gmail.com>
2024-02-22 23:57:04 -0600

    updated dataset

--- run_eval.sh
+++ run_eval.sh
@@ -2,6 +2,6 @@ output_path="/acegpt_7_chat_results"
 
 lm_eval --model hf \
     --model_args parallelize=FreedomIntelligence/AceGPT-7B-chat \
-    --tasks aratrust \
+    --tasks araTrust \
     --batch_size 1 \
     --write_out \
